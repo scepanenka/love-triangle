2449558de9ebf2d503a9d79f80894e73b2b8d73d
scepanenka <scepanenka@gmail.com>
2019-03-04 09:30:39 +0300

    feat: implement function for getting count of love triangles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = function getLoveTrianglesCount(preferences) {
+  let trianglesCount = 0;
+  let a, b, c;
+
+  for (var i = 0; i < preferences.length; i++) {
+    a = b = c = -1;
+
+    if (preferences[i] > 0) {
+      a = preferences[i];
+    }
+    if (preferences[a - 1] > 0) {
+      b = preferences[a - 1];
+    }
+    if (preferences[b - 1] > 0) {
+      c = preferences[b - 1];
+    }
+
+    if (c - 1 == i && (a != b && b != c)) {
+      preferences[a - 1] = preferences[b - 1] = preferences[c - 1] = -1;
+      trianglesCount++;
+    }
+  }
+  return trianglesCount;
+}
